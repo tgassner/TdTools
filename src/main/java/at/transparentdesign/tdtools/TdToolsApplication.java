cda30c73b86769f15dd79168f109b5f0ac6ade25
tgassner <thomas.gassner@gmx.net>
2025-05-22 16:36:59 +0200

    also renaming xfml file

--- src/main/java/at/transparentdesign/tdtools/TdToolsApplication.java
+++ src/main/java/at/transparentdesign/tdtools/TdToolsApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class TdToolsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TdToolsApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TdToolsApplication.class.getResource("tdToolsMain-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("TD Tools");
         stage.setScene(scene);
